3ae8bec8bfb28b02eb4a3ddc5623ddd2c6ef806d
Agustin Calderon <agustincl@gmail.com>
2014-02-14 13:22:29 +0100

    interfaces inicio

---
+++ CrudFile/public/functions.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Rename file with a counter
+ *  
+ * @param string $original_name
+ * @param string $path
+ * @return string $final_name
+ */
+function renameFile($original_name, $path)
+{
+
+}
+
+/**
+ * Upload file to directory
+ * @param string $name
+ * @param string $path
+ * @param array $fileData $_FILES data
+ * @return null
+ */
+function uploadFile($name, $path, $fileData)
+{
+
+}
+
+/**
+ * Insert data into text file
+ * @param array $data array with data
+ * @param string $filename
+ * @return null
+ */
+function insert2Txt($data, $filename)
+{
+	
+}
+
+
+
