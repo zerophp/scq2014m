5e76d790c56135454a26530438de407fcc7f62e8
Agustin Calderon <agustincl@gmail.com>
2014-02-14 13:52:49 +0100

    Functions for models

--- CrudFile/public/functions.php
+++ CrudFile/public/functions.php
@@ -7,9 +7,22 @@
  * @param string $path
  * @return string $final_name
  */
-function renameFile($original_name, $path)
+function renameFile($photo_name, $destino)
 {
-
+	// Averiguar el nombre de la foto
+	$partes_ruta = pathinfo($destino."/".$photo_name);
+		
+	//Mientras que el nombre del fichero exista en destino
+	$a=0;
+	while(file_exists($destino."/".$photo_name))
+	{
+		// Aumentar nombre de fichero
+		$a++;
+		// Hasta obtener un nombre valido
+		$photo_name=$partes_ruta['filename'].
+		"_".$a.".".$partes_ruta['extension'];
+	}	
+	return $photo_name;
 }
 
 /**
@@ -21,7 +34,11 @@ function renameFile($original_name, $path)
  */
 function uploadFile($name, $path, $fileData)
 {
-
+	// Subir la foto
+	move_uploaded_file($fileData['tmp_name'],
+						$path."/".$name
+	);
+	return null;
 }
 
 /**
@@ -30,9 +47,30 @@ function uploadFile($name, $path, $fileData)
  * @param string $filename
  * @return null
  */
-function insert2Txt($data, $filename)
+function insert2Txt($datafile, $filename)
 {
+	$arraysalida=array();
+	// Para cada elemento del array POST
+	foreach ($datafile as $value)
+	{
+		// Si no es array
+		if(!is_array($value))
+			// Enviar a arraysalida
+			$arraysalida[]=$value;
+		// Si es array
+		else
+			// Dividir por pipes y
+			// enviar a arraysalida
+			$arraysalida[]=implode('|',$value);
+	}
 	
+	// Separar por comas
+	$data=implode(',',$arraysalida)."\n";
+		
+	// Escribir(agregar) a un archivo de texto
+	file_put_contents($filename, $data, FILE_APPEND);
+	
+	return null;
 }
 
 
--- CrudFile/public/procesar.php
+++ CrudFile/public/procesar.php
@@ -14,59 +14,20 @@ print_r($_FILES);
 echo "</pre>";
 
 
-
-
-
-// Averiguar el nombre de la foto
-$photo_name = $_FILES['photo']['name'];
-$destino = $_SERVER['DOCUMENT_ROOT'];
-$partes_ruta = pathinfo($destino."/".$photo_name);
-
-
-
-//Mientras que el nombre del fichero exista en destino
-$a=0;
-while(file_exists($destino."/".$photo_name))
-{
-	// Aumentar nombre de fichero
-	$a++;
-	// Hasta obtener un nombre valido
-	$photo_name=$partes_ruta['filename'].
-				"_".
-				$a.
-				".".
-				$partes_ruta['extension'];
-}
+include ('functions.php');
 
 // Inyectar nombre_final en post.
-$_POST[]=$photo_name;
 
-$arraysalida=array();
-// Para cada elemento del array POST
-foreach ($_POST as $value)
-{
-	// Si no es array
-	if(!is_array($value))
-		// Enviar a arraysalida
-		$arraysalida[]=$value;		
-	// Si es array
-	else	
-		// Dividir por pipes y
-		// enviar a arraysalida
-		$arraysalida[]=implode('|',$value);	
-}
-	
-// Separar por comas
-$data=implode(',',$arraysalida)."\n";
+$photo_name = renameFile($_FILES['photo']['name'],  
+					$_SERVER['DOCUMENT_ROOT']);
+$destino = $_SERVER['DOCUMENT_ROOT'];
+
+$_POST[]= $photo_name;
 
 // Subir la foto
-move_uploaded_file($_FILES['photo']['tmp_name'],
-		$destino."/".$photo_name
-);
+uploadFile($photo_name, $destino, $_FILES['photo']);
 
-
-// Escribir(agregar) a un archivo de texto
-file_put_contents('usuarios.txt', $data, FILE_APPEND);
+insert2Txt($_POST, 'usuarios.txt');
 
 // Saltar a tabla de usuarios
 // header('Location: http://formularios.local/usuarios.php');
